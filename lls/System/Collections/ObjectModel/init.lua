889ecf031c78fc1970ba221e0176f4c58b3b0147
Wintermourn <gladosnwheatley@gmail.com>
2026-07-14 06:06:08 -0500

    move lls to separate repository

--- lls/System/Collections/ObjectModel/init.lua
+++
@@ -1,8 +0,0 @@
----@meta
-
----@class System.Collections.ObjectModel
-local SysColObM = {}
-
----@alias ReadOnlyCollection System.Collections.ObjectModel.ReadOnlyCollection
----@class System.Collections.ObjectModel.ReadOnlyCollection<T> : {[int]: T}
----@field Count int
